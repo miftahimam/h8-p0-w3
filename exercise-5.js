c1371257e61c1aebd301ab81bef15262088f4d6e
miftahul imam <imammatot@gmail.com>
2018-10-31 10:02:42 +0700

    update tugas rabu

--- exercise-5.js
+++ exercise-5.js
@@ -1,40 +1,13 @@
 function palindrome(str) {
-  var tr = false;
-    var gnjl = 0;
-  str = str.toLowerCase();
-  str = str.replace(/[\W_]+/g,'');
-   
-  var strs = str.split("");
-  var strl = strs.length;
-  var cntr = Math.ceil(strl/2);
-  if(strl%2==1) gnjl = 1;
-     
-    
-  
-  var str1 = [];
-  var str2 = [];
-  for(var i=0;i<=cntr-1;i++){
-        str1.push(strs[i]);
+    var len = str.length;
+    for ( var i = 0; i < Math.floor(len/2); i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
     }
-    for(var i=cntr-1;i<strl;i++){
-        str2.push(strs[i]);
-  }
-  if(gnjl){ 
- 
-  }else{
-      str2.splice(0,1);
-  }
-  str1 = str1.join('');
-  str2 = str2.reverse().join('');
-  if(str1 == str2) tr =true;
-   
-  
-  return tr;
+    return true;
 }
 
-// TEST CASES
-console.log(palindrome('katak')); // true
-console.log(palindrome('blanket')); // false
-console.log(palindrome('civic')); // true
-console.log(palindrome('kasur rusak')); // true
-console.log(palindrome('mister')); // false
+console.log(palindrome('malam'))
+console.log(palindrome('jika'))
+console.log(palindrome('katak'))
